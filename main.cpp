5e1b53757061aac7961de8b1a2cfb813ed7d40f4
Carlos Martinez <cbmartinez5@yahoo.com>
2024-10-24 16:30:37 -0700

    fixed

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
 int main()
 {
 
 
     cout << "Greetings!\n";
     cout << "Hello World!\n";
+    say("hi");
 
 
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
