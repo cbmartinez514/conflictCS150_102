95a6f1ecf5d498427c80e2be80adfb39766cfaae
cbmartinez514 <cbmartinez5@yahoo.com>
2024-10-24 23:24:38 +0000

    Createdadd function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
-
+int add (int a, int b);
 int main()
 {
 
     cout << "Hello World!\n";
+    cout << "The sum of 3 and 4 is " << add(3,4) << endl;
 
     return 0;
+}
+
+int add (int a, int b)
+{
+    return a+b;
 }
